b853afc5bf6b8baeb450cf6bdab41ac9918f5d3c
lcnhut <nhut.le@nfq.asia>
2022-05-17 00:28:37 +0700

    feat: responsive mobile - works section

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -34,3 +34,26 @@ overlay.onclick = (e) => {
     overlay.classList.remove("active");
   }
 };
+
+// Swiper - Works section
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
