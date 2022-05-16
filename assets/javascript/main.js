b2de9758267b86b24c6794240b09052ea34c2235
lcnhut <nhut.le@nfq.asia>
2022-05-16 17:55:05 +0700

    feat: navbar mobile

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,25 @@
-const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".navbar__menu");
+const openBtn = document.querySelector(".navbar__button--open");
+const closeBtn = document.querySelector(".navbar__button--close");
+const overlay = document.querySelector(".overlay");
+const items = document.querySelectorAll(".navbar__item a");
 
-console.log(navbar);
+openBtn.onclick = () => {
+  menu.classList.add("active");
+  openBtn.classList.add("hide");
+  overplay.classList.add("active");
+};
+
+closeBtn.onclick = () => {
+  menu.classList.remove("active");
+  openBtn.classList.remove("hide");
+  overplay.classList.remove("active");
+};
+
+items.forEach((item) => {
+  item.onclick = () => {
+    menu.classList.remove("active");
+    openBtn.classList.remove("hide");
+    overlay.classList.remove("active");
+  };
+});
