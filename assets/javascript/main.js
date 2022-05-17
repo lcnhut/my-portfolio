bb580972e612facea7053a34e710edb1a2d85a81
lcnhut <nhut.le@nfq.asia>
2022-05-17 14:35:33 +0700

    feat: active navbar item when scroll to section

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,7 +4,30 @@ const openBtn = document.querySelector(".navbar__button--open");
 const closeBtn = document.querySelector(".navbar__button--close");
 const overlay = document.querySelector(".navbar__overlay");
 const items = document.querySelectorAll(".navbar__item a");
+const sections = document.querySelectorAll("section");
 
+// Active navbar menu when scroll
+window.addEventListener("scroll", () => {
+  let currentSection = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (pageYOffset > sectionTop - 50 - sectionHeight / 3) {
+      currentSection = section.getAttribute("class");
+    }
+  });
+
+  items.forEach((item) => {
+    item.classList.remove("active");
+    if (item.getAttribute("href").slice(1) == currentSection.slice(8)) {
+      item.classList.add("active");
+    }
+  });
+});
+
+// Navbar mobile
 openBtn.onclick = () => {
   menu.classList.add("active");
   openBtn.classList.add("hide");
@@ -49,6 +72,10 @@ const swiper = new Swiper(".swiper", {
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
+  },
+
+  autoplay: {
+    delay: 2500,
   },
 
   // Navigation arrows
