b98ba1a8b8ab447661eb11d179b8bd10e2c4d175
lcnhut <nhut.le@nfq.asia>
2022-05-19 16:18:29 +0700

    feat: fix bugs responsive

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -73,12 +73,6 @@ const swiper = new Swiper(".swiper", {
     delay: 2500,
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
   // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
